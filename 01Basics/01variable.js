729e58a891f3a07dbc7fee9730186fffc7424c05
Faizan1882 <148028410+Faizan1882@users.noreply.github.com>
2024-08-24 10:41:55 +0000

    da

---
+++ 01Basics/01variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail= "faizan63@gmail.com"
+var accountPassword ="12345"
+accontCity = "Ashti"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "abcd34@.com"
+accountPassword = "2546123"
+accontCity = "Beed"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accontCity,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01Basics/02datatypes.js
@@ -0,0 +1,17 @@
+let name = "faizan"
+let age = 22
+let isloggedIn = false
+
+// number => 2 to the power 53
+// bigin
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undifined = if you dont assing any value 
+// symbol = to find unique 
+
+
+// object 
+
+console.log(typeof undefined); // undifind
+console.log(typeof null); // object
