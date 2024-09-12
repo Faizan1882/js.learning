e2e2c811ec8a01a603abb9d6efad3b80b0d501ac
Faizan1882 <148028410+Faizan1882@users.noreply.github.com>
2024-09-12 05:11:50 +0000

    this is comparision

--- 01Basics/03ConversionOperation.js
+++ 01Basics/03ConversionOperation.js
@@ -19,4 +19,30 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let SomeNumber = 63
 
 let stringNumber = String(SomeNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+/////////// operations/////////////
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/2);
+//  console.log(2%3);
+
+let str1 = "faizan "
+let str2 = "sayyed"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+
---
+++ 01Basics/04comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>= 2);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+// console.log("2"> 1);
+// console.log("02"> 1);
+
+// console.log(null> 0);
+// console.log(null== 0);
+// console.log(null>= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === this checks its datatype also and the show the output
+
+console.log("2"=== 2);
