a1587b7f4ad75172fc581d1e53077f353c5d6a81
Faizan1882 <148028410+Faizan1882@users.noreply.github.com>
2024-09-13 04:57:23 +0000

    datatype summary

---
+++ 01Basics/datatypesummary.js
@@ -0,0 +1,44 @@
+ // premetive 
+
+ // 7 types
+
+//  string 
+//  number 
+//  boolean 
+//  null 
+//  undefined
+//  symbol 
+//  BigInt
+
+const score = 100  
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp= null 
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+
+//const bigNumber = 323151545464565n
+
+const heros = ["shaktiman","naagraj","doga"]
+ 
+let myobj ={
+    name: "faizan",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello faizan")
+}
+
+console.log( typeof anotherId );
+
+// Reference (non premetive)
+
+// array 
+// objects
+// functions
